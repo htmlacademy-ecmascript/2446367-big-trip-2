f1a7ec4611e249a80682e9d060b58ac099073005
Sasha Kandaurow <al.kandaurow@yandex.ru>
2024-10-30 01:16:33 +0700

    mock-data added

---
+++ src/mock/mock-destinations.js
@@ -0,0 +1,80 @@
+
+
+const mockDestinations = [
+  {
+    'id': 'cfe416cq-10xa-ye10-8077-2fs9a01edcab-001',
+    'description': 'Belgrade is the capital and largest city of Serbia.',
+    'name': 'Belgrade',
+    'pictures': [
+      {
+        'src': 'https://loremflickr.com/248/152?random=3',
+        'description': 'Belgrade parliament building'
+      },
+      {
+        'src': 'https://loremflickr.com/248/152?random=5',
+        'description': 'Belgrade building'
+      },
+    ]
+  },
+  {
+    'id': 'cfe416cq-10xa-ye10-8077-2fs9a01edcab-002',
+    'description': 'Bor is a city and the administrative center of the Bor District in the Timok Valley in eastern Serbia.',
+    'name': 'Bor',
+    'pictures': []
+  },
+  {
+    'id': 'cfe416cq-10xa-ye10-8077-2fs9a01edcab-003',
+    'description': 'Novi Pazar is a city located in southwestern Serbia.',
+    'name': 'Novi Pazar',
+    'pictures': [
+      {
+        'src': 'https://loremflickr.com/248/152?random=3',
+        'description': 'Novi building'
+      },
+      {
+        'src': 'https://loremflickr.com/248/152?random=4',
+        'description': 'Novi building'
+      },
+    ]
+  },
+  {
+    'id': 'cfe416cq-10xa-ye10-8077-2fs9a01edcab-004',
+    'description': 'Loznica is a city located in the Mačva District of western Serbia.',
+    'name': 'Loznica',
+    'pictures': [
+      {
+        'src': 'https://loremflickr.com/248/152?random=10',
+        'description': 'Loznica building'
+      },
+      {
+        'src': 'https://loremflickr.com/248/152?random=11',
+        'description': 'Loznica building'
+      },
+      {
+        'src': 'https://loremflickr.com/248/152?random=12',
+        'description': 'Loznica building'
+      },
+    ]
+  },
+  {
+    'id': 'cfe416cq-10xa-ye10-8077-2fs9a01edcab-005',
+    'description': 'Novi Sad is the second largest city in Serbia after the capital Belgrade',
+    'name': 'Novi Sad',
+    'pictures': [
+      {
+        'src': 'https://loremflickr.com/248/152?random=20',
+        'description': 'Novi Sad building'
+      },
+      {
+        'src': 'https://loremflickr.com/248/152?random=21',
+        'description': 'Novi Sad'
+      },
+      {
+        'src': 'https://loremflickr.com/248/152?random=22',
+        'description': 'Novi Sad'
+      },
+    ]
+  },
+];
+
+export { mockDestinations };
---
+++ src/view/waypoint-edit-view.js
@@ -0,0 +1,174 @@
+import { createElement } from '../render.js';
+import { WAYPOINT_TYPE } from '../data.js';
+import { getElementById, getElementByType, capitalizeFirstLetter } from '../utils.js';
+
+function createTypeTemplate (type, checkedType, id) {
+  const isChecked = checkedType === type ? 'checked' : false;
+
+  return (`
+    <div class="event__type-item">
+      <input id="event-type-${type}-${id}" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}" ${isChecked}>
+      <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-${id}">${capitalizeFirstLetter(type)}</label>
+    </div>
+  `);
+}
+
+function createOfferTemplate (offer, checkedOffers) {
+  const { id, title, price } = offer;
+  const isChecked = checkedOffers.includes(id) ? 'checked' : '';
+
+  return (`
+    <div class="event__offer-selector">
+      <input class="event__offer-checkbox  visually-hidden" id="${id}" type="checkbox" name="${id}" ${isChecked}>
+      <label class="event__offer-label" for="${id}">
+        <span class="event__offer-title">${title}</span>
+        &plus;&euro;&nbsp;
+        <span class="event__offer-price">${price}</span>
+      </label>
+    </div>
+  `);
+}
+
+function createOffersListTemplate ({offers}, checkedOffers) {
+  if (offers.length !== 0) {
+    return (`
+      <section class="event__section  event__section--offers">
+        <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+        <div class="event__available-offers">
+          ${offers.map((offer) => createOfferTemplate(offer, checkedOffers)).join('')}
+        </div>
+      </section>
+    `);
+  }
+
+  return '';
+}
+
+function createPhotoTemplate (photo) {
+  const { src, description } = photo;
+  return (`
+    <img class="event__photo" src=${src} alt=${description}>
+  `);
+}
+
+function createPhotoContainerTemplate (pictures) {
+  if (pictures.length > 0) {
+    return (`
+      <div class="event__photos-container">
+        <div class="event__photos-tape">
+          ${pictures.map((item) => createPhotoTemplate(item)).join('')}
+        </div>
+      </div>
+    `);
+  }
+
+  return '';
+}
+
+function createDestinationTemplate (destination) {
+  const { description, pictures } = destination || {};
+
+  if (description.length > 0 || pictures.length > 0) {
+    return (`
+      <section class="event__section  event__section--destination">
+        <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+        <p class="event__destination-description">${description}</p>
+
+        ${createPhotoContainerTemplate(pictures)}
+      </section>
+    `);
+  }
+
+  return '';
+}
+
+function createWaypointEditTemplate (waypoints, offers, destinations) {
+  const { id, type, basePrice, offers: checkedOffers, destination: waypointDestination } = waypoints;
+  const filterOfferByType = getElementByType(offers, type);
+  const filterDestinationById = getElementById(destinations, waypointDestination);
+  const { name } = filterDestinationById;
+
+  return (`
+    <li class="trip-events__item">
+      <form class="event event--edit" action="#" method="post">
+        <header class="event__header">
+
+          <div class="event__type-wrapper">
+            <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
+              <span class="visually-hidden">Choose event type</span>
+              <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+            </label>
+            <input class="event__type-toggle  visually-hidden" id="event-type-toggle-${id}" type="checkbox">
+
+            <div class="event__type-list">
+              <fieldset class="event__type-group">
+                <legend class="visually-hidden">Event type</legend>
+                  ${WAYPOINT_TYPE.map((item) => createTypeTemplate(item, type, id)).join('')}
+              </fieldset>
+            </div>
+          </div>
+
+            <div class="event__field-group  event__field-group--destination">
+              <label class="event__label  event__type-output" for="event-destination-${id}">
+                ${type}
+              </label>
+              <input class="event__input  event__input--destination" id="event-destination-${id}" type="text" name="event-destination" value="${name}" list="destination-list-${id}">
+              <datalist id="destination-list-${id}">
+                ${destinations.map((destination) => `<option value=${destination.name}></option>`)}
+              </datalist>
+            </div>
+
+            <div class="event__field-group  event__field-group--time">
+              <label class="visually-hidden" for="event-start-time-${id}">From</label>
+              <input class="event__input  event__input--time" id="event-start-time-${id}" type="text" name="event-start-time" value="10/07/2019 10:00">
+              &mdash;
+              <label class="visually-hidden" for="event-end-time-${id}">To</label>
+              <input class="event__input  event__input--time" id="event-end-time-${id}" type="text" name="event-end-time" value="11/07/2019 10:00">
+            </div>
+
+            <div class="event__field-group  event__field-group--price">
+              <label class="event__label" for="event-price-${id}">
+                <span class="visually-hidden">Price</span>
+                &euro;
+              </label>
+              <input class="event__input  event__input--price" id="event-price-${id}" type="text" name="event-price" value="${basePrice}">
+            </div>
+
+            <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+            <button class="event__reset-btn" type="reset">Delete</button>
+            <button class="event__rollup-btn" type="button">
+              <span class="visually-hidden">Open event</span>
+            </button>
+          </header>
+          <section class="event__details">
+            ${createOffersListTemplate(filterOfferByType, checkedOffers)}
+            ${createDestinationTemplate(filterDestinationById)}
+          </section>
+        </form>
+      </li>
+  `);
+}
+
+export default class WaypointEdit {
+  constructor({ waypoints, offers, destinations }) {
+    this.waypoints = waypoints;
+    this.offers = offers;
+    this.destinations = destinations;
+  }
+
+  getTemplate() {
+    return createWaypointEditTemplate(this.waypoints, this.offers, this.destinations);
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
